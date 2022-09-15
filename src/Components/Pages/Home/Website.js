a7a930030e16a1cb3ab92b257a8d5280db3f3460
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-09-15 23:51:51 +0600

    paragraph

--- src/Components/Pages/Home/Website.js
+++ src/Components/Pages/Home/Website.js
@@ -7,7 +7,12 @@ const Website = () => {
       {/* web analaytics */}
       <div className="web-box">
         <h2 className=" text-5xl font-bold">Website Analaytics</h2>
-        <h2 className="text-3xl">trafic</h2>
+        <h2 className="text-3xl text-primary">trafic</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas
+          qui doloremque facilis possimus eos placeat corrupti blanditiis iusto
+          iste!
+        </p>
       </div>
       {/* stack overflow */}
       <div></div>
